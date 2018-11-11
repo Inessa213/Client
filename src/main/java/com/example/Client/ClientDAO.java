cd8f9648065091daadc6a3ea7b3fe1f316a626f6
Inessa213 <i.pavliuk213@gmail.com>
2018-11-11 14:31:34 +0200

    Initial commit

---
+++ src/main/java/com/example/Client/ClientDAO.java
@@ -0,0 +1,36 @@
+package com.example.Client;
+
+
+import Model.ClientInfo;
+import Mapper.ClientMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+
+@Repository
+public class ClientDAO extends JdbcDaoSupport {
+
+    @Autowired
+    public ClientDAO(DataSource dataSource) {
+        this.setDataSource(dataSource);
+    }
+
+
+    public List<ClientInfo> getClient(String first_name) {
+
+        String sql = "Select * from client where first_name = '"+first_name+"'";
+        Object[] params = new Object[]{};
+        ClientMapper mapper = new ClientMapper();
+        List<ClientInfo> list = this.getJdbcTemplate().query(sql, params, mapper);
+
+        return list;
+
+    }
+}
+
+
+
---
+++ src/main/java/com/example/Client/TESTController.java
@@ -0,0 +1,33 @@
+package com.example.Client;
+
+
+import Model.ClientInfo;
+import Model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TESTController {
+
+    @Autowired
+    private ClientDAO clientDAO;
+
+    @RequestMapping(
+            value = "/test.action",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.POST)
+
+    @ResponseBody
+    public List<ClientInfo> testRisk(@RequestBody final Greeting request){
+
+
+        List<ClientInfo> list = clientDAO.getClient(request.getFirst_name());
+
+        return list;
+    }
+
+}
