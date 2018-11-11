54832fd96ff00849816158ece0f4bc1655644dd1
Inessa213 <i.pavliuk213@gmail.com>
2018-11-11 21:08:10 +0200

    Add

--- src/main/java/Mapper/ClientMapper.java
+++ src/main/java/Mapper/ClientMapper.java
@@ -13,12 +13,11 @@ public class ClientMapper implements RowMapper<ClientInfo> {
     @Override
     public ClientInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-
-
         Long id = rs.getLong("id");
         String firstName = rs.getString("first_name");
         String lastName = rs.getString("last_name");
         String patronumic = rs.getString("patronumic");
+
         int age = rs.getInt("age");
 
         return new ClientInfo(id, firstName, lastName, patronumic, age);
